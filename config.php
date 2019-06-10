e3139ad70425f6da0846f5fbdde728e087f0372f
Urvi Awasthi <urviawasthi@gmail.com>
2019-06-10 03:48:46 +0000

    Test

--- config.php
+++ config.php
@@ -2,9 +2,7 @@
         //connecting to the server  
 
         //assigns the environmental variables
-        $dbhost =  $_ENV['DATABASE_HOST'];
-        $dbuser =  $_ENV['DATABASE_USER'];
-        $dbpass =   $_ENV['DATABASE_PASSWORD'];
-        $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+        $dbstring =  getenv('CLEARDB_DATABASE_URL');
+        $conn = mysqli_connect($dbstring);
 
 ?> 
